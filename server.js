c6d398aff6a661c23c81a353ce59948cc8fb687a
ClaudeBrilliant <claudennyongesa7@gmail.com>
2025-01-24 18:36:09 +0300

    static file middleware

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import notFound from './middleware/notFound.js';
 import errorHandler from './middleware/error.js';
 import logger from './middleware/logger.js';
 import posts from './routes/post.js';
 const app = express();
 const port = process.env.PORT || 3000;
+
+//get the dir name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Body parser middleware
 //it parses incoming request with JSON payloads
@@ -17,7 +22,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))// allows to parse url encoded data
 
 // setup static folder
-//app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 //Routes
 app.use('/api/posts', posts);
