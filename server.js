e947557030b65e811fa454381579c700c1b2fe2a
ClaudeBrilliant <claudennyongesa7@gmail.com>
2025-01-24 10:29:50 +0300

    routes

--- server.js
+++ server.js
@@ -1,43 +1,20 @@
-const express = require('express');
+import express from 'express';
+import path from 'path';
+import posts from './routes/post.js';
 const app = express();
-const path = require('path');
 const port = process.env.PORT || 3000;
+
+// Body parser middleware
+//it parses incoming request with JSON payloads
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))// allows to parse url encoded data
 
 // setup static folder
 //app.use(express.static(path.join(__dirname, 'public')))
 
-let posts = [
-    {
-        id: 1,
-        title: "Post 1",
-        content: "This is the first post"
-    },
-    {
-        id: 2,
-        title: "Post 2",
-        content: "This is the second post"
-    },
-    {
-        id: 3,
-        title: "Post 3",
-        content: "This is the third post"
-    }   
-]
-//get all posts
-app.get('/api/posts', function(req, res){ 
-    const limit = req.query.limit
-    if(!isNaN(limit) && limit > 0){
-        posts = posts.slice(0, limit)
-    } else {  
-    res.json(posts)}
-})
-
-//get single post
-app.get('/api/posts/:id', function(req, res){
-    const id = req.params.id
-    const post = posts.find(post => post.id == id)
-    res.json(post)
-})
+//Routes
+app.use('/api/posts', posts);
 
 
 
