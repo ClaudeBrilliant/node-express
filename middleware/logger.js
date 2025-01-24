57cc184f9e9413340b856bd06e097671d298b144
ClaudeBrilliant <claudennyongesa7@gmail.com>
2025-01-24 18:38:49 +0300

    color utility

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,9 +1,13 @@
+import colors from "colors";
 //middleware
 //The middleware function has access to the request and response objects
 
 const logger = function (req, res, next) {
   console.log(
     `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`
+    [
+      'blue'
+    ]
   );
   next();
 };
