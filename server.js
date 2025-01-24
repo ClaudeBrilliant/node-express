b74dd40815892ecabb23bc585574387f0275e6cd
ClaudeBrilliant <claudennyongesa7@gmail.com>
2025-01-24 06:43:36 +0300

    post method

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port = process.env.PORT || 3000;
 
 // setup static folder
 //app.use(express.static(path.join(__dirname, 'public')))
@@ -22,11 +23,24 @@ let posts = [
         content: "This is the third post"
     }   
 ]
+//get all posts
 app.get('/api/posts', function(req, res){ 
+    const limit = req.query.limit
+    if(!isNaN(limit) && limit > 0){
+        posts = posts.slice(0, limit)
+    } else {  
+    res.json(posts)}
+})
 
+//get single post
+app.get('/api/posts/:id', function(req, res){
+    const id = req.params.id
+    const post = posts.find(post => post.id == id)
+    res.json(post)
 })
 
 
-app.listen(3000, function(){
-console.log("Server is running on port 3000")
+
+app.listen(port, function(){
+console.log(`Server is running on port ${port}`)
 })
